22e90cac6455f57ae47dfd99499b2bbb9d3c560e
justjoe19 <joeholl1979@icloud.com>
2023-07-10 15:56:38 -0400

    first commit

---
+++ test_sum_unittest.py
@@ -0,0 +1,19 @@
+# Joe Hollenbach
+# SDEV 220, M05 Programming Assignment
+# 7/10/2023
+
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
+# second test failed    
+
